ed1d64e3fde1c30d14ec99f71b538b3fab59d3b7
wvallone <william.vallone@ags-it.com>
2026-03-17 16:52:47 +0100

    varie

--- frontend/src/environments/environments.prod.ts
+++ frontend/src/environments/environments.prod.ts
@@ -1,8 +1,3 @@
-// export const environment = {
-//   production: true,
-//   apiUrl: 'http://gekohub.com:8000/api'  // dominio reale del backend
-// };
-
 export const environment = {
   production: true,
   apiUrl: 'http://backend:8000/api' , // backend = nome servizio Docker
